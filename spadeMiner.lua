8842bdc9d700f21ae7838283e54531c61089184a
Spade <spadeorsp8@users.noreply.github.com>
2024-08-12 15:05:14 -0400

    Refactor miner

--- spadeMiner.lua
+++ spadeMiner.lua
@@ -1,9 +1,9 @@
 --[[
-@title Spade Miner BETA
+@title Spade Miner
 @author Spade
 
-* Update ROCKS table with IDs of actual rocks you want to mine
 * Start script near rocks you want to mine
+* Select rock you want to mine from menu
 
 Note: Supports drinking perfect juju mining potions and recharging GOTE if porters are in inventory
 --]]
@@ -11,90 +11,120 @@ Note: Supports drinking perfect juju mining potions and recharging GOTE if porte
 local API = require('api')
 
 local MAX_IDLE_TIME_MINUTES = 10
-local POTIONS = {32769, 32771, 32773, 32775}
--- local ROCKS = { 112963, 112964 }
-local ROCKS = { 113206, 113207, 113208 }
+local POTIONS = { 32769, 32771, 32773, 32775 }
 local HIGHLIGHTS = { 7164, 7165 }
+local PORTERS = { 51490, 29285, 29283, 29281, 29279, 29277, 29275 }
+local menu = API.CreateIG_answer()
+local imguiBackground = API.CreateIG_answer()
 
 API.SetDrawTrackedSkills(true)
 API.SetMaxIdleTime(MAX_IDLE_TIME_MINUTES)
 
-local function keepGOTEcharged()
+local function setupMenu()
+    local uniqueTable = {}
+    local objects = API.ReadAllObjectsArray({0, 12}, {-1}, {})
+    for _, o in ipairs(objects) do
+        if o.Name and (string.find(o.Name, 'rock') or o.Name == 'Seren stone')and o.Distance <= 50 then
+            uniqueTable[o.Name] = true
+        end
+    end
+
+    imguiBackground.box_name = "imguiBackground"
+    imguiBackground.box_start = FFPOINT.new(1, 60, 0)
+    imguiBackground.box_size = FFPOINT.new(400, 100, 0)
+    imguiBackground.colour = ImColor.new(10, 13, 29)
+
+    menu.box_name = "Spade Miner"
+    menu.box_start = FFPOINT.new(1, 60, 0)
+    menu.box_size = FFPOINT.new(400, 0, 0)
+    menu.stringsArr = {}
+    menu.colour = ImColor.new(10, 13, 29)
+
+    for r, _ in pairs(uniqueTable) do
+        table.insert(menu.stringsArr, r)
+    end
+    table.sort(menu.stringsArr)
+
+    API.DrawSquareFilled(imguiBackground)
+    API.DrawComboBox(menu, false)
+end
+
+local function chargeGOTE()
     local buffStatus = API.Buffbar_GetIDstatus(51490, false)
     local stacks = tonumber(buffStatus.text)
 
-    local function findporters()
-        local portersIds = {51490, 29285, 29283, 29281, 29279, 29277, 29275}
-        local porters = API.CheckInvStuff3(portersIds)
-        local foundIdx = -1
-        for i, value in ipairs(porters) do
-            if tostring(value) == '1' then
-                foundIdx = i
-                break
-            end
-        end
-        if foundIdx ~= -1 then
-            local foundId = portersIds[foundIdx]
-            if foundId <= 51490 then
-                return foundId
-            else
-                return nil
-            end
-        else
-            return nil
+    local porterId = nil
+    for _, id in ipairs(PORTERS) do
+        if API.InvItemcount_1(id) > 0 then
+            porterId = id
+            break
         end
     end
-    
-    if stacks and stacks <= 50 and findporters() then
+
+    if porterId and stacks and stacks <= 50 then
         print ("Recharging GOTE")
-        
-        -- Assumes GOTE is in slot 2 of action bar
-        API.DoAction_Interface(0xffffffff, 0xae06, 5, 1430, 77, -1, API.OFF_ACT_GeneralInterface_route)
-        API.RandomSleep2(600,600,600)
+        API.DoAction_Ability("Grace of the elves", 5, API.OFF_ACT_GeneralInterface_route)
+        API.RandomSleep2(500, 250, 500)
     end
 end
 
 local function takeMiningPot()
-    if API.Buffbar_GetIDstatus(32773).conv_text <= 1 then
-        for _, pot in ipairs(POTIONS) do
-            if API.InvItemcount_1(pot) > 0 then
-                print("Drinking potion!")
-                API.DoAction_Inventory1(pot, 0, 1, API.OFF_ACT_GeneralInterface_route)
-                break
-            end
+    if API.Buffbar_GetIDstatus(32773).conv_text > 1 then
+        return
+    end
+
+    for _, pot in ipairs(POTIONS) do
+        if API.InvItemcount_1(pot) > 0 then
+            print("Drinking potion!")
+            API.DoAction_Inventory1(pot, 0, 1, API.OFF_ACT_GeneralInterface_route)
+            break
         end
     end
 end
 
-local function FindHl(objects, maxdistance, highlight)
-    for _, obj in ipairs(API.GetAllObjArray1(objects, maxdistance, {0, 12})) do
-        for _, hl in ipairs(API.GetAllObjArray1(highlight, maxdistance, {4})) do
-            if math.abs(obj.Tile_XYZ.x - hl.Tile_XYZ.x) <= 1 and math.abs(obj.Tile_XYZ.y - hl.Tile_XYZ.y) <= 1 then
-                return obj
+local function getShinyRock(rocks, maxDistance)
+    local shinyRock = nil
+    local shortestDist = maxDistance
+    for _, rock in ipairs(rocks) do
+        for _, hl in ipairs(API.GetAllObjArray1(HIGHLIGHTS, maxDistance, {4})) do
+            if API.Math_DistanceF(rock.Tile_XYZ, hl.Tile_XYZ) < shortestDist then
+                shinyRock = rock
+                shortestDist = API.Math_DistanceF(rock.Tile_XYZ, hl.Tile_XYZ)
             end
         end
     end
 
-    return nil
+    return shinyRock
 end
+
+setupMenu()
 
 clickedRock = nil
+selectedRock = nil
 while API.Read_LoopyLoop() do
     API.DoRandomEvents()
-
     takeMiningPot()
-    keepGOTEcharged()
+    chargeGOTE()
 
-    local shinyRock = FindHl(ROCKS, 50, HIGHLIGHTS)
-    if shinyRock then        
-        if not clickedRock or API.Math_DistanceF(clickedRock.Tile_XYZ, shinyRock.Tile_XYZ) ~= 0 then
-            API.RandomSleep2(500, 1000, 1500)
-            API.DoAction_Object_Direct(0x3a, API.OFF_ACT_GeneralObject_route0, shinyRock)
-            clickedRock = shinyRock
-        end
-    else
-        if not API.CheckAnim(25) then
-            API.DoAction_Object1(0x3a, API.OFF_ACT_GeneralObject_route0, ROCKS, 50)
+    if (menu.return_click) then
+        menu.return_click = false
+        print("Selected " .. menu.string_value)
+        selectedRock = menu.string_value
+    end
+
+    if selectedRock then
+        local rocks = API.GetAllObjArrayInteract_str({ selectedRock }, 50, { 0, 12 })
+        local shinyRock = getShinyRock(rocks, 50)
+        if shinyRock then
+            if not clickedRock or API.Math_DistanceF(clickedRock.Tile_XYZ, shinyRock.Tile_XYZ) ~= 0 then
+                API.RandomSleep2(500, 1000, 1500)
+                API.DoAction_Object_Direct(0x3a, API.OFF_ACT_GeneralObject_route0, shinyRock)
+                clickedRock = shinyRock
+            end
+        else
+            if not API.CheckAnim(25) and #rocks > 0 then
+                API.DoAction_Object_Direct(0x3a, API.OFF_ACT_GeneralObject_route0, rocks[math.random(1, #rocks)])
+            end
         end
     end
 
