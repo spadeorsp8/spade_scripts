3d6910c4f5cc25dbea4b91013f22c45f352c9ce2
Spade <spadeorsp8@users.noreply.github.com>
2024-08-29 09:18:57 -0400

    Add multi-spot support to Abyss script

--- spadeAbyss.lua
+++ spadeAbyss.lua
@@ -1,6 +1,11 @@
 --[[
-@title Spade Abyss - WORK IN PROGRESS
+@title Spade Abyss
 @author Spade
+
+* Have food in inventory and put "Eat food" ability on ability bar
+* Put War's Retreat teleport on ability bar
+* Configure custom loot button in RS3 settings, or set LOOT to nil to disable looting
+* Start script after entering the Abyss
 --]]
 
 local API = require("api")
@@ -8,37 +13,68 @@ local MAX_IDLE_TIME_MINUTES = 10
 
 API.SetDrawTrackedSkills(true)
 API.SetMaxIdleTime(MAX_IDLE_TIME_MINUTES)
-
--- safe spot: API.DoAction_Tile(WPOINT.new(3048,4805,0))
--- training spot: API.DoAction_Tile(WPOINT.new(3059,4812,0))
--- reset spot: API.DoAction_Tile(WPOINT.new(3058,4844,0))
+math.randomseed(os.time())
 
 local LOOT = { 592, 7937 }
 local NPCS = { 2265, 2264, 2263 }
-local TRAINING_SPOT = FFPOINT.new(3059, 4812, 0)
+local STARTING_SPOT = FFPOINT.new(3040 + math.random(-5, 5), 4809 + math.random(-5, 5), 0)
+local SAFE_SPOT = FFPOINT.new(3048, 4805, 0)
+local TRAINING_DATA = {
+    SW = {
+        trainingSpots = { FFPOINT.new(3030, 4807, 0), FFPOINT.new(3029, 4807, 0) },
+        resetSpot = {3019, 4844},
+    },
+    SE = {
+        trainingSpots = { FFPOINT.new(3059, 4812, 0), FFPOINT.new(3059,4814,0) },
+        resetSpot = {3058, 4844},
+    },
+}
 
---[[
-Training spot 1 alt:
-[16:41:38:144] API.DoAction_Tile(WPOINT.new(3059,4814,0))
+local function maintainHealth()
+    if API.GetHPrecent() > 50 then
+        return
+    end
 
-Training spot 2:
-[16:37:49:011] API.DoAction_Tile(WPOINT.new(3030,4807,0))
-[16:37:52:885] API.DoAction_Tile(WPOINT.new(3029,4807,0))
+    print("Eating food!")
+    if not API.DoAction_Ability_check("Eat Food", 1, API.OFF_ACT_GeneralInterface_route, true, true) then
+        API.DoAction_Ability("Retreat Teleport", 1, API.OFF_ACT_GeneralInterface_route)
+        API.Write_LoopyLoop(false)
+        return
+    end
 
-Reset spot 2:
-[16:39:55:095] API.DoAction_Tile(WPOINT.new(3019,4844,0))
-]]
-
-local function moveToTrainingSpot()
-    print("Running to training spot")
-    API.DoAction_TileF(TRAINING_SPOT)
     API.RandomSleep2(1000, 250, 500)
-    while API.ReadPlayerMovin2() and API.Read_LoopyLoop() do
+end
+
+local function waitForStillness()
+    API.RandomSleep2(1000, 250, 500)
+    while (API.CheckAnim(2) or API.ReadPlayerMovin2()) and API.Read_LoopyLoop() do
+        maintainHealth()
         API.RandomSleep2(1000, 250, 500)
     end
-    API.RandomSleep2(1000, 250, 500)
+end
 
+local function moveToTrainingSpot(trainingSpots)
+    print("Running to training spot.")
+    API.DoAction_TileF(trainingSpots[math.random(1, #trainingSpots)])
+    waitForStillness()
+
+    API.RandomSleep2(1000, 250, 500)
     API.DoAction_NPC(0x2a, API.OFF_ACT_AttackNPC_route, NPCS, 2)
+end
+
+local function loot(trainingData)
+    if not API.LootWindowOpen_2() then
+        -- Open loot window and move back to training spot
+        if API.DoAction_Loot_w(LOOT, 3, API.PlayerCoordfloat(), 10) then
+            API.RandomSleep2(1000, 250, 500)
+            moveToTrainingSpot(trainingData.trainingSpots)
+        end
+    else
+        if math.random(1, 100) <= 5 then
+            -- Press custom loot button
+            API.DoAction_Interface(0x24, 0xffffffff, 1, 1622, 30, -1, API.OFF_ACT_GeneralInterface_route)
+        end
+    end
 end
 
 local function playerNearTile(targetTile, dist)
@@ -53,70 +89,52 @@ local function playerNearTile(targetTile, dist)
     return false
 end
 
-local function getCurrentWorld()
-    local worldPtr = 0x00D82B00
-    local offsets = {0x78, 0x9C8, 0x0, 0x20, 0x8}
-
-    local val = API.Mem_Read_uint64(API.Get_RSExeStart() + worldPtr)
-    for _, offset in pairs(offsets) do
-        val = API.Mem_Read_uint64(val + offset)
+local function chooseTrainingData()
+    for k, v in pairs(TRAINING_DATA) do
+        if not playerNearTile(v.trainingSpots[1], 15) then
+            print(string.format("Choosing %s training spot.", k))
+            return v
+        end
     end
-    return val
+
+    return nil
 end
 
-print(string.format("%x", getCurrentWorld()))
+if #API.GetAllObjArray1(NPCS, 50, {1}) <= 0 then
+    print("No Abyssal NPCs detected, please start in the Abyss!")
+    return
+end
 
--- print(playerNearTile(TRAINING_SPOT, 15))
+if API.Dist_FLP(STARTING_SPOT) >= 20 then
+    print("Running to starting spot.")
+    API.DoAction_TileF(STARTING_SPOT)
+    waitForStillness()
+end
 
--- TODO: Add more than one training spot, and automatically move to it based on if a player is nearby or not
+local TDATA = chooseTrainingData()
+if not TDATA then
+    -- TODO: Hop worlds automatically
+    print("Neither training spot is open, please hop worlds and try again!")
+    API.DoAction_TileF(SAFE_SPOT)
+    return
+end
 
-moveToTrainingSpot()
+moveToTrainingSpot(TDATA.trainingSpots)
 while API.Read_LoopyLoop() do
     if API.GetGameState2() ~=3 or not API.PlayerLoggedIn() then
         print("Bad game state, exiting.")
         break
     end
 
-    if API.GetHPrecent() < 50 then
-        print("Eating food!")
-
-        if not API.DoAction_Ability_check("Eat Food", 1, API.OFF_ACT_GeneralInterface_route, true, true) then
-            API.DoAction_Ability("Retreat Teleport", 1, API.OFF_ACT_GeneralInterface_route)
-            break
-        end
-
-        API.RandomSleep2(1000, 250, 500)
-    end
-
-    -- TODO: Convert to state machine to get rid of while loop sleeps
-    if not API.LootWindowOpen_2() then
-        -- Open loot window and move back to training spot
-        if API.DoAction_Loot_w(LOOT, 3, API.PlayerCoordfloat(), 10) then
-            API.RandomSleep2(1000, 250, 500)
-            moveToTrainingSpot()
-        end
-    else
-        if math.random(1, 100) <= 5 then
-            -- Loot custom
-            API.DoAction_Interface(0x24, 0xffffffff, 1, 1622, 30, -1, API.OFF_ACT_GeneralInterface_route)
-        end
-    end
-    
-
+    maintainHealth()
+    if LOOT then loot(TDATA) end
     if not API.LocalPlayer_IsInCombat_() then
-        print("Running to reset spot")
-        local resetTile = FFPOINT.new(3058 + math.random(-5, 5), 4844 + math.random(0, 5), 0)
+        print("Running to reset spot.")
+        local resetTile = FFPOINT.new(TDATA.resetSpot[1] + math.random(-5, 5), TDATA.resetSpot[2] + math.random(0, 5), 0)
         API.DoAction_TileF(resetTile)
-        API.RandomSleep2(1000, 250, 500)
-        API.DoAction_Ability_check("Surge", 1, API.OFF_ACT_GeneralInterface_route, true, true)
-        API.RandomSleep2(1000, 250, 500)
-        API.DoAction_TileF(resetTile)
-        API.RandomSleep2(1000, 250, 500)
-        while (API.CheckAnim(2) or API.ReadPlayerMovin2()) and API.Read_LoopyLoop() do
-            API.RandomSleep2(1000, 250, 500)
-        end
 
-        moveToTrainingSpot()
+        waitForStillness()
+        moveToTrainingSpot(TDATA.trainingSpots)
     end
 
     API.RandomSleep2(1000, 250, 500)
