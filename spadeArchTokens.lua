78bff422d8e87a45925572feb17f48e8bd815b0d
Spade <spadeorsp8@users.noreply.github.com>
2024-11-12 14:19:40 -0500

    Fix arch token metrics

--- spadeArchTokens.lua
+++ spadeArchTokens.lua
@@ -13,6 +13,10 @@ local MAX_IDLE_TIME_MINUTES = 10
 local ARCH_FOCUS = 7307
 local COMPLETE_TOME = 49976
 local BANKER = 9710
+local TOTEM = 56975
+local MINE = 56977
+local RING = 56973
+local GATESTONE = 56971
 local PORTERS = { 51490, 29285, 29283, 29281, 29279, 29277, 29275 }
 local SPOTS = { 130307, 130309 }
 
@@ -21,12 +25,16 @@ if API.ScriptDialogWindow2("Starting Location", {"Castle hall rubble", "Tunnelli
     spotIdx = 2
 end
 
-API.SetDrawLogs(false)
+local totemCount = 0
+local mineCount = 0
+local ringCount = 0
+local gatestoneCount = 0
+
 API.SetDrawTrackedSkills(true)
 API.SetMaxIdleTime(MAX_IDLE_TIME_MINUTES)
 
 local followSprite = (API.ScriptDialogWindow2("Follow Sprite?", {"Yes", "No"}, "Start", "Close").Name == "Yes")
-local clickedRemainsTile = nil
+local clickedSpotTile = nil
 local artifactFoundInterface = {
     InterfaceComp5.new(1189, 2, -1, 0),
 }
@@ -42,19 +50,19 @@ local function waitForStillness()
     end
 end
 
-local function getFocusRemains(remains, maxDistance)
-    local focusRemains = nil
+local function getSpriteSpot(spots, maxDistance)
+    local spriteSpot = nil
     local shortestDist = maxDistance
-    for _, r in ipairs(remains) do
+    for _, r in ipairs(spots) do
         for _, hl in ipairs(API.GetAllObjArray1({ ARCH_FOCUS }, maxDistance, {4})) do
             if API.Math_DistanceF(r.Tile_XYZ, hl.Tile_XYZ) < shortestDist then
-                focusRemains = r
+                spriteSpot = r
                 shortestDist = API.Math_DistanceF(r.Tile_XYZ, hl.Tile_XYZ)
             end
         end
     end
 
-    return focusRemains
+    return spriteSpot
 end
 
 local function getPorter()
@@ -86,6 +94,11 @@ local function chargeGOTE()
 end
 
 local function bank()
+    totemCount = totemCount + API.InvItemcount_1(TOTEM)
+    mineCount = mineCount + API.InvItemcount_1(MINE)
+    ringCount = ringCount + API.InvItemcount_1(RING)
+    gatestoneCount = gatestoneCount + API.InvItemcount_1(GATESTONE)
+
     -- Destroy complete tomes so we can load preset
     if API.DoAction_Inventory1(COMPLETE_TOME, 0, 8, API.OFF_ACT_GeneralInterface_route2) then
         API.RandomSleep2(2000, 500, 1000)
@@ -113,42 +126,51 @@ local function bank()
     spotIdx = (spotIdx % #SPOTS) + 1
     print(string.format("Switching to spot %d", SPOTS[spotIdx]))
 
-    clickedRemainsTile = nil
+    clickedSpotTile = nil
     return true
 end
 
 local function excavate()
     chargeGOTE()
     if artifactFoundInterfacePresent() and followSprite then
-        clickedRemainsTile = nil
+        clickedSpotTile = nil
     end
 
-    local remains = API.GetAllObjArrayInteract({ SPOTS[spotIdx] }, 50, { 0, 1, 12 })
+    local spots = API.GetAllObjArrayInteract({ SPOTS[spotIdx] }, 50, { 0, 1, 12 })
 
-    local focusRemains = nil
+    local spriteSpot = nil
     if followSprite then
-        focusRemains = getFocusRemains(remains, 50)
+        spriteSpot = getSpriteSpot(spots, 50)
     end
 
-    if focusRemains then
-        if not clickedRemainsTile or API.Math_DistanceF(clickedRemainsTile, focusRemains.Tile_XYZ) ~= 0 then
+    if spriteSpot then
+        if not clickedSpotTile or API.Math_DistanceF(clickedSpotTile, spriteSpot.Tile_XYZ) ~= 0 then
             API.RandomSleep2(500, 1000, 1500)
-            API.DoAction_Object_Direct(0x2, API.OFF_ACT_GeneralObject_route0, focusRemains)
-            clickedRemainsTile = focusRemains.Tile_XYZ
+            API.DoAction_Object_Direct(0x2, API.OFF_ACT_GeneralObject_route0, spriteSpot)
+            clickedSpotTile = spriteSpot.Tile_XYZ
         end
     else
-        if not (API.CheckAnim(50) or API.ReadPlayerMovin2()) and #remains > 0 then
-            local targetRemainsTile = clickedRemainsTile
-            if not targetRemainsTile then
-                targetRemainsTile = remains[math.random(1, #remains)].Tile_XYZ
+        if not (API.CheckAnim(50) or API.ReadPlayerMovin2()) and #spots > 0 then
+            local targetSpotTile = clickedSpotTile
+            if not targetSpotTile then
+                targetSpotTile = spots[math.random(1, #spots)].Tile_XYZ
             end
 
-            API.DoAction_Object2(0x2, API.OFF_ACT_GeneralObject_route0, { SPOTS[spotIdx] }, 50, WPOINT.new(targetRemainsTile.x, targetRemainsTile.y, 0))
-            clickedRemainsTile = targetRemainsTile
+            API.DoAction_Object2(0x2, API.OFF_ACT_GeneralObject_route0, { SPOTS[spotIdx] }, 50, WPOINT.new(targetSpotTile.x, targetSpotTile.y, 0))
+            clickedSpotTile = targetSpotTile
         end
     end
 
     return true
+end
+
+local function getCollectionsPerHour()
+    local collections = math.min(ringCount + API.InvItemcount_1(RING),
+                                 gatestoneCount + API.InvItemcount_1(GATESTONE),
+                                 totemCount + API.InvItemcount_1(TOTEM),
+                                 mineCount + API.InvItemcount_1(MINE))
+    local runtimeHrs = API.ScriptRuntime() / 3600.0
+    return collections / runtimeHrs
 end
 
 while API.Read_LoopyLoop() do
@@ -160,23 +182,19 @@ while API.Read_LoopyLoop() do
     API.DoRandomEvents()
 
     if API.InvFull_() and not bank() then
-        --[[
-            TODO: if not bank:
-            1. Open bank
-            2. Count each artifact, take the smallest count for number of groups to fix
-            3. Teleport to Anachronia
-            4. Grab up to 6 of each artifact from bank
-            5. Interact with workbench to fix artifacts
-            6. Donate artifacts
-            7. Repeat 4-6 until there are no more artifacts to fix
-            8. Teleport to Daemonheim using ring
-                * API.DoAction_Interface(0xffffffff,15707,2,1430,77,-1,API.OFF_ACT_GeneralInterface_route)
-                * API.DoAction_Ability("Ring of kinship", 2, API.OFF_ACT_GeneralInterface_route)
-        ]] 
         break
     elseif not excavate() then
         break
     end
+
+    local metrics = {
+        {"Engraved ring of kinship", ringCount + API.InvItemcount_1(RING)},
+        {"Castle gatestone", gatestoneCount + API.InvItemcount_1(GATESTONE)},
+        {"Exploratory totem", totemCount + API.InvItemcount_1(TOTEM)},
+        {"Excavator portal mine", mineCount + API.InvItemcount_1(MINE)},
+        {"Collections per hour", getCollectionsPerHour()}
+    }
+    API.DrawTable(metrics)
 
     API.RandomSleep2(1000, 1000, 1000)
 end
